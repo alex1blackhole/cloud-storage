38a21eae2ef9e495f0203ab2d0f026ab7699eea4
frontend1 <isaynason@gmail.com>
2022-09-07 16:53:00 +0300

    added api for downloading

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -7,6 +7,7 @@ const fileController = require('../controller/fileController')
 
 router.post('', authMiddleware, fileController.createDir)
 router.post('/upload', authMiddleware, fileController.uploadFiles)
+router.get('/download', authMiddleware, fileController.downloadFile)
 router.get('', authMiddleware, fileController.getFiles)
 
 
